ec7dd87273c74e487c62b0a912de084f487e6473
Michael Carr <mcarr2468@gmail.com>
2022-10-05 18:24:17 -0500

    fixed CORS error

--- functions/1-hello.js
+++ functions/1-hello.js
@@ -4,6 +4,10 @@ const guy = {name:'bobo',age:34}
 
 exports.handler = async (event, context) => {
     return {
+        // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Access-Control-Allow-Origin
+        headers:{
+            'Access-Control-Allow-Origin':'*',
+        },
         statusCode:200,
         // body has to be a string, can stringify an object,arrays,ect...
         body:JSON.stringify(guy)
